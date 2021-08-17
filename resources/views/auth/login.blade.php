cbfbc66b4c16230d797d4ddf35a159b2436346e5
KOBAYASHI5963 <kobayan567666@gmail.com>
2021-08-17 14:32:02 +0900

    ログイン機能追加

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center">
+        <h1>Login</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 offset-sm-3">
+
+        <form method="POST" action="{{ route('login') }}">
+        <div class="form-group row">
+                  <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                      <div class="col-md-6">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                      </div>
+            </div>
+
+            <div class="form-group row">
+                   <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                      <div class="col-md-6">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                      </div>
+            </div>
+
+            <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+                                </div>
+            </div>
+            
+            <p class="mt-2"><a href="{{ route('signup.get') }}">Sign up now!</a></p>
+        </div>
+    </div>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Microposts</h1>
+            <a href="{{route ('signup.get')}}">sign up now!!</a>
         </div>
     </div>
 @endsection
